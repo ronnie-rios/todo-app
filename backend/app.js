bdb71959a639c6ea707cb105fe9db10463f71ea0
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-05 15:22:05 -0500

    mongoose connected and routes controller

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 9004;
 
+const todoController = require('./routes/todoController');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose.connect('mongodb://localhost/tododb2');
 
 app.get('/', (req, res) => {
     res.send('hello friend')
 })
+
+app.use('/todo, todoController');
 
 app.listen(port, () => {
     console.log(`example app running on ${port}`)
